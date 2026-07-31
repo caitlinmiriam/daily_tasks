2c56ecc260248c4a16da9e3715076c2b0aab8c8d
caitlinmiriam <caitlinmiriam@gmail.com>
2026-07-31 09:23:10 +0530

    Add twoSum method to Solution class
    Implemented twoSum method to find indices of two numbers that add up to the target.

--- TwoSumII.java
+++ TwoSumII.java
@@ -61,3 +61,22 @@ class Solution {
 }
 
 // You can avoid the second break by returning immediately when you find the answe
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int i=0;
+        int j=numbers.length-1;
+        while(i<j){
+            if(numbers[i]+numbers[j]==target){
+                return new int[]{i+1,j+1};
+            }
+            else if(numbers[i]+numbers[j]<target){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
