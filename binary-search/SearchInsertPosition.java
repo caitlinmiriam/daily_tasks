5a351490f1d48fa1e09f9bd48bf176bd669bf72e
caitlinmiriam <caitlinmiriam@gmail.com>
2026-08-11 10:04:17 +0530

    Add binary search implementation for searchInsert
    Implemented binary search approach for searchInsert method.

--- binary-search/SearchInsertPosition.java
+++ binary-search/SearchInsertPosition.java
@@ -14,7 +14,8 @@
 // Example 3:
 // Input: nums = [1,3,5,6], target = 7
 // Output: 4
-  
+
+// Lowerbound approach
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int low=0;
@@ -32,6 +33,26 @@ class Solution {
         }
         return ans;
     }
+}
+
+// TC: O(log n)
+// SC: O(1)
+
+// Idk what to name this approach
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target) 
+              return mid;
+            else if(nums[mid] < target) 
+              low = mid + 1;
+            else 
+              high = mid - 1;
+        }
+        return low;
+    }
 }
 
 // TC: O(log n)
